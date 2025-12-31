0a1b4489dbf3d239ec7bf14bfc376e9c86ad7a04
mohdminhaj12w33-dot <mohdminhaj12w33@gmail.com>
2025-12-31 22:05:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-//your JS code here. If required.
+const buttons = document.querySelectorAll(".btn");
+const stopButton = document.querySelector(".stop");
+
+const sounds = {};
+
+// Load sounds
+buttons.forEach(button => {
+  const soundName = button.dataset.sound;
+  sounds[soundName] = new Audio(`sounds/${soundName}.mp3`);
+
+  button.addEventListener("click", () => {
+    stopAllSounds();
+    sounds[soundName].play();
+  });
+});
+
+// Stop all sounds
+function stopAllSounds() {
+  for (let sound in sounds) {
+    sounds[sound].pause();
+    sounds[sound].currentTime = 0;
+  }
+}
+
+// Stop button
+stopButton.addEventListener("click", stopAllSounds);
